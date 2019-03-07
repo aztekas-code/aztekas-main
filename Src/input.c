52dbabefe653afd75d5fe15c4c7a94458f9b81b2
aztekas-code <aaguayo@astro.unam.mx>
2019-03-06 18:49:35 -0600

    Start of documentation with Doxygen

--- Src/input.c
+++ Src/input.c
@@ -1,16 +1,9 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+/**
+ * @file input.c
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * @author Emilio Tejeda
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @brief Important input parameters for \a aztekas.
  */
 
 //Do not erase any of these libraries//
@@ -19,7 +12,7 @@
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
-#include"./Headers/main.h"
+#include"main.h"
 
 FILE *paramfile;
 
