b28cf0d5a3a7fa1cbd4a71fb9a812ab90e3d0697
aztekas-code <aaguayo@astro.unam.mx>
2019-03-28 22:05:07 -0600

    Update

--- Src/HD-sph/q2uvector.c
+++ Src/HD-sph/q2uvector.c
@@ -47,6 +47,26 @@ int funct_Q2U(double *a, double *uu)
          }
       }
    }
+   else if(dim == 4)
+   {
+      for(i = 0; i <= Nx1; i++)
+      {
+         for(j = 0; j <= Nx2; j++)
+         {
+            D  = uu[c2(0,i,j)];
+            E  = uu[c2(1,i,j)];
+            m1 = uu[c2(2,i,j)];
+            m2 = uu[c2(3,i,j)];
+            m3 = uu[c2(4,i,j)];
+    
+            a[c2(0,i,j)] = D;
+            a[c2(1,i,j)] = ((2*K-2)*D*E+(1-K)*pow(m3,2.0)+(1-K)*pow(m2,2.0)+(1-K)*pow(m1,2.0))/(2*D);
+            a[c2(2,i,j)] = m1/D;
+            a[c2(3,i,j)] = m2/D;
+            a[c2(4,i,j)] = m3/D;
+         }
+      }
+   }
    if(dim == 3)
    {
       for(i = 0; i <= Nx1; i++)
