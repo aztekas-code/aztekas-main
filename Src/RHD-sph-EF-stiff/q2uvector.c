f30e1cfc7828da6539cb5153e500a3820d00d35e
aaguayoo <aaguayoo92@ciencias.unam.mx>
2019-05-02 20:07:19 -0500

    Add RHD-3p1

---
+++ Src/RHD-sph-EF-stiff/q2uvector.c
@@ -0,0 +1,152 @@
+#include<stdio.h>
+#include<math.h>
+#include"../Headers/vector.h"
+#include"../Headers/main.h"
+
+int funct_Q2U(double *a, double *uu)
+{
+   int i, j, k;
+   double D, t, m=0, n=0, o=0;
+   double R, SS;
+   double dWu, dWv, dWw;
+   double dhn, dhp;
+   double theta, theta_0;
+   double f, derf, h, derh, lor;
+
+   R  = sqrt(pow(x2,2.0)+pow(x1,2.0));
+
+   if(dim == 1)
+   {
+      for(i = 0; i <= Nx1; i++)
+      {
+         D = uu[c1(0,i)];
+         t = uu[c1(1,i)];
+         m = uu[c1(2,i)];
+         n = 0;
+         o = 0;
+
+         x1 = X1[i];
+         x2 = 0;
+         x3 = 0;
+
+         SS = ((pow(m,2.0)*pow(x1,3.0)+pow(n,2.0)*x1+2*MM*pow(n,2.0))*pow(sin(x2),2.0)+pow(o,2.0)*x1+2*MM*pow(o,2.0))/((pow(x1,3.0)+2*MM*pow(x1,2.0))*pow(sin(x2),2.0));
+
+         theta_0 = 0.0;
+         f = 2.0;
+
+         while (fabs(f) > 0.0000001)
+         {
+            h    = 1.0 + (K / (K - 1.0)) * theta_0;
+            lor  = sqrt(1.0 + SS / pow(D*h,2.0));
+            derh = K / (K - 1.0);
+
+            f    = h * lor - (theta_0 / lor) - (t / D) - 1.0;
+            derf = (1.0 / lor) * (derh - 1.0 - theta_0 * ((lor * lor - 1.0) / (lor * lor)) * (derh / h));
+
+            theta = theta_0 - f / derf;
+            theta_0 = theta;
+         }
+
+         h   = 1.0 + (K / (K - 1.0)) * theta_0;
+         lor  = sqrt(1.0 + SS / pow(D*h,2.0));
+
+         a[c1(0,i)] = D / lor;
+         a[c1(1,i)] = D*h*lor - t - D;
+         a[c1(2,i)] = m / (D * h * lor);
+      }
+   }
+   else if(dim == 2)
+   {
+      for(i = 0; i <= Nx1; i++)
+      {
+         for(j = 0; j <= Nx2; j++)
+         {
+            D = uu[c2(0,i,j)];
+            t = uu[c2(1,i,j)];
+            m = uu[c2(2,i,j)];
+            n = uu[c2(3,i,j)];
+            o = 0;
+
+            x1 = X1[i];
+            x2 = X2[j];
+            x3 = 0;
+
+            SS = ((pow(m,2.0)*pow(x1,3.0)+pow(n,2.0)*x1+2*MM*pow(n,2.0))*pow(sin(x2),2.0)+pow(o,2.0)*x1+2*MM*pow(o,2.0))/((pow(x1,3.0)+2*MM*pow(x1,2.0))*pow(sin(x2),2.0));
+
+            theta_0 = 0.0;
+            f = 2.0;
+
+            while (fabs(f) > 0.000001)
+            {
+               h    = 0.00001 + (K / (K - 1.0)) * theta_0;
+               lor  = sqrt(1.0 + SS / pow(D*h,2.0));
+               derh = K / (K - 1.0);
+
+               f    = h * lor - (theta_0 / lor) - (t / D) - 1.0;
+               derf = (1.0 / lor) * (derh - 1.0 - theta_0 * ((lor * lor - 1.0) / (lor * lor)) * (derh / h));
+
+               theta = theta_0 - f / derf;
+               theta_0 = theta;
+            }
+
+            h   = 0.00001 + (K / (K - 1.0)) * theta;
+            lor  = sqrt(1.0 + SS / pow(D*h,2.0));
+
+            a[c2(0,i,j)] = D / lor;
+            a[c2(1,i,j)] = D*h*lor - t - D;
+            a[c2(2,i,j)] = m / (D * h * lor);
+            a[c2(3,i,j)] = n / (D * h * lor);
+         }
+      }
+   }
+   if(dim == 3)
+   {
+      for(i = 0; i <= Nx1; i++)
+      {
+         for(j = 0; j <= Nx2; j++)
+         {
+            for(k = 0; k <= Nx3; k++)
+            {
+               D = uu[c3(0,i,j,k)];
+               t = uu[c3(1,i,j,k)];
+               m = uu[c3(2,i,j,k)];
+               n = uu[c3(3,i,j,k)];
+               o = uu[c3(4,i,j,k)];
+
+               x1 = X1[i];
+               x2 = X2[j];
+               x3 = X3[k];
+
+               SS = ((pow(m,2.0)*pow(x1,3.0)+pow(n,2.0)*x1+2*MM*pow(n,2.0))*pow(sin(x2),2.0)+pow(o,2.0)*x1+2*MM*pow(o,2.0))/((pow(x1,3.0)+2*MM*pow(x1,2.0))*pow(sin(x2),2.0));
+
+               theta_0 = 0.0;
+               f = 2.0;
+
+               while (fabs(f) > 0.000001)
+               {
+                  h    = 1.0 + (K / (K - 1.0)) * theta_0;
+                  lor  = sqrt(1.0 + SS / pow(D*h,2.0));
+                  derh = K / (K - 1.0);
+
+                  f    = h * lor - (theta_0 / lor) - (t / D) - 1.0;
+                  derf = (1.0 / lor) * (derh - 1.0 - theta_0 * ((lor * lor - 1.0) / (lor * lor)) * (derh / h));
+
+                  theta = theta_0 - f / derf;
+                  theta_0 = theta;
+               }
+
+               h   = 1.0 + (K / (K - 1.0)) * theta;
+               lor  = sqrt(1.0 + SS / pow(D*h,2.0));
+
+               a[c3(0,i,j,k)] = D / lor;
+               a[c3(1,i,j,k)] = D*h*lor - t - D;
+               a[c3(2,i,j,k)] = m / (D * h * lor);
+               a[c3(3,i,j,k)] = n / (D * h * lor);
+               a[c3(4,i,j,k)] = o / (D * h * lor);
+            }
+         }
+      }
+   }
+
+   return 0;
+}
---
+++ Src/RHD-sph-EF/dnmatrix.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+#include"../Headers/vector.h"
+#include"../Headers/main.h"
+
+int funct_Dn(double *a, double *uu)
+{
+   int i;
+   double n, p, u=0, v=0, w=0;
+   double R, V;
+   double yt22, Bt2, Vt2, c;
+   n = uu[0];
+   p = uu[1];
+   u = uu[2];
+   if(dim >= 2){v = uu[3];}
+   if(dim == 3){w = uu[4];}
+
+   R    = x1;
+   V    = ((x1+2*MM)*pow(w,2.0)+(x1+2*MM)*pow(sin(x2),2.0)*pow(v,2.0)+pow(x1,3.0)*pow(sin(x2),2.0)*pow(u,2.0))/((pow(x1,3.0)+2*MM*pow(x1,2.0))*pow(sin(x2),2.0));
+   yt22 = 1/pow(x1,2.0);
+   Bt2  = 0;
+   Vt2  = v/pow(x1,2.0);
+   c    = sqrt(((pow(K,2.0)-K)*p)/(K*p+(K-1)*n));
+
+   a[0] = -((c)*sqrt(x1)*sqrt(((pow(V,2.0)-V)*pow(c,2.0)-V+1)*yt22+(1-V)*pow(Vt2,2.0)*pow(c,2.0)+(V-1)*pow(Vt2,2.0))+(Bt2*V*pow(c,2.0)-Bt2)*sqrt(x1+2*MM)+(Vt2-Vt2*pow(c,2.0))*sqrt(x1))/((V*pow(c,2.0)-1)*sqrt(x1+2*MM));
+   a[1] = ((c)*sqrt(x1)*sqrt(((pow(V,2.0)-V)*pow(c,2.0)-V+1)*yt22+(1-V)*pow(Vt2,2.0)*pow(c,2.0)+(V-1)*pow(Vt2,2.0))+(Bt2-Bt2*V*pow(c,2.0))*sqrt(x1+2*MM)+(Vt2*pow(c,2.0)-Vt2)*sqrt(x1))/((V*pow(c,2.0)-1)*sqrt(x1+2*MM));
+   a[2] = -(Bt2*sqrt(x1+2*MM)-Vt2*sqrt(x1))/sqrt(x1+2*MM);
+
+   return 0;
+}
