f71d464db80f2bbf2912ed49f2a42893360ff799
aaguayoo <aaguayoo92@ciencias.unam.mx>
2019-06-15 15:52:26 -0500

    Prueba3 Update

---
+++ SIMULATIONS/HD/Rayleigh-Taylor/Headersfiles/param_01.h
@@ -0,0 +1,11 @@
+// Define boundaries TRUE = 1 , FALSE = 0
+// Set as cond_X whereas
+// cond : {outflow,reflective,periodic,inflow}
+// X : {x1max,x1min,x2max,x2min,x3max,x3min}
+
+#define periodic_x1max 1
+#define periodic_x1min 1
+#define reflective_x2max 1
+#define reflective_x2min 1
+
+#define limiter 'C'
---
+++ SIMULATIONS/HD/Rayleigh-Taylor/param.h
@@ -0,0 +1,11 @@
+// Define boundaries TRUE = 1 , FALSE = 0
+// Set as cond_X whereas
+// cond : {outflow,reflective,periodic,inflow}
+// X : {x1max,x1min,x2max,x2min,x3max,x3min}
+
+#define periodic_x1        TRUE
+#define reflective_x2max   TRUE
+#define reflective_x2min   TRUE
+
+#define RECONST      WENO5
+#define riemann      2
