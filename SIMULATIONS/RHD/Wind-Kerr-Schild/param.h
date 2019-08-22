23ab7e1adcc6fc9adba1e5843ac331bac4c12fab
aztekas-code <aaguayo@astro.unam.mx>
2019-08-22 18:19:49 -0500

    modified Kerr-Schild metric in spherical coordinates implemented.

---
+++ SIMULATIONS/RHD/Wind-Kerr-Schild/param.h
@@ -0,0 +1,23 @@
+#include"macros.h"
+
+// Define boundaries TRUE = 1 , FALSE = 0
+// Set as cond_X whereas
+// cond : {outflow,reflective,periodic,inflow}
+// X : {x1max,x1min,x2max,x2min,x3max,x3min}
+
+#define outflow_x1max      TRUE
+#define outflow_x1min      TRUE
+#define reflective_x2max   TRUE
+#define reflective_x2min   TRUE
+
+#define RECONST            MC
+#define FLUX               HLL
+#define GRID               UNIFORM
+
+#define Mach      5.0
+#define vinf      0.5
+
+double density_0, pressure_0, velocity_0;
+
+#define Black_Hole_Mass    1.0
+#define Black_Hole_Spin    0.0
