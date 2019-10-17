72845df94e9bf989f7b1bfb759d9efb609219c3b
Alejandro <aaguayoo92@ciencias.unam.mx>
2019-10-17 17:12:57 -0500

    Kerr-Schild si sale

--- SIMULATIONS/RHD/Wind-Kerr-Schild/user_param.h
+++ SIMULATIONS/RHD/Wind-Kerr-Schild/user_param.h
@@ -5,7 +5,7 @@
 // cond : {outflow,reflective,periodic,inflow}
 // X : {x1max,x1min,x2max,x2min,x3max,x3min}
 
-#define POLAR              FALSE
+#define POLAR              TRUE
 
 #define outflow_x1max      TRUE
 #define outflow_x1min      TRUE
@@ -18,10 +18,10 @@
 
 #define RECONST            MC
 #define FLUX               HLL
-#define GRID               UNIFORM
+#define GRID               LOGMESH
 
 #define Black_Hole_Mass    1.0
-#define Black_Hole_Spin    0.0
+#define Black_Hole_Spin    1.1
 #define Mach               5.0
 #define vinf               0.5
 
