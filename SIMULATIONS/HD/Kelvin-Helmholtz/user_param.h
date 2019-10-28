44749a45f46f6176f07f14ebbe863b9aa43fac60
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2019-10-28 15:44:09 -0600

    Makefile JET-HD

--- SIMULATIONS/HD/Kelvin-Helmholtz/user_param.h
+++ SIMULATIONS/HD/Kelvin-Helmholtz/user_param.h
@@ -2,7 +2,7 @@
  * File Name : user_param.h
  * Description : aztekas user parameters header file for Kelvin-Helmholtz
  * Creation Date : 27-09-2019
- * Last Modified : 27-09-2019 00:08:27
+ * Last Modified : 28-10-2019 15:43:29
  * Created By : Alejandro Aguayo-Ortiz
  */
 
