90ee64998e2c923d1dd5a98b5ef761f87c2aca6a
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2019-11-08 15:29:57 -0600

    Change setup

---
+++ prueba/initial.c
@@ -0,0 +1,17 @@
+/* 
+ *  aztekas initial module
+ *  Date of creation: 08-11-2019 15:18:37
+ *  author: Alejandro Aguayo-Ortiz 
+ */
+#include"main.h"
+
+void Initial()
+{
+   int n, i, j, k, cell;
+
+   //Initialize time
+   grid.time = 0.0;
+
+   //Initialize dt
+   dt = 0.0;
+}
---
+++ prueba/param.h
@@ -0,0 +1,10 @@
+#include<macros.h>
+
+#define outflow_x1max        TRUE
+#define outflow_x1min        TRUE
+#define outflow_x2max        TRUE
+#define outflow_x2min        TRUE
+
+#define RECONST               MC
+#define FLUX                  HLL
+#define GRID                  UNIFORM
