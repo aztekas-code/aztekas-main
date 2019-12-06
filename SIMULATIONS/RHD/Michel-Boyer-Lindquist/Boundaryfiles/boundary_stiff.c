4039bc5a753363f4868baec294903d4e9a37a033
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2019-12-06 12:51:17 -0600

    Modifications in SIMULATIONS

--- SIMULATIONS/RHD/Michel-Boyer-Lindquist/Boundaryfiles/boundary_stiff.c
+++ SIMULATIONS/RHD/Michel-Boyer-Lindquist/Boundaryfiles/boundary_stiff.c
@@ -1,22 +1,14 @@
 /*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * File Name : user_boundaries.c
+ * Description : aztekas boundaries module for Michel Stiff EoS problem
+ * Creation Date : 05-12-2019
+ * Last Modified : 05-12-2019 13:08:56
+ * Created By : Alejandro Aguayo-Ortiz
  */
 
-//Do not erase any of these libraries//
 #include"main.h"
 
-int Boundaries(double *B)
+void User_Boundaries(double *B)
 {
    int i, j, k, n, cell;
    double r, theta;
@@ -99,6 +91,4 @@ int Boundaries(double *B)
    }
 
 #endif
-
-   return 0;
 }
