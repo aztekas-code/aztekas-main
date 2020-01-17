23389b64476ebbfa3157c0980fcf4e2d597da582
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2020-01-17 09:54:31 -0600

    New user_boundaries.c (the one in HD) for RHD/Shock-Tube.

--- SIMULATIONS/RHD/Shock-Tube/user_boundaries.c
+++ SIMULATIONS/RHD/Shock-Tube/user_boundaries.c
@@ -1,27 +1,23 @@
 /*
  * File Name : user_boundaries.c
- * Description : aztekas boundaries module for Relativistic Shock-Tube
- * Creation Date : 28-09-2019
- * Last Modified : 28-10-2019 17:28:24
- * Created By : Alejandro Aguayo-Oritz
+ * Description : aztekas boundaries module for Shock-Tube
+ * Creation Date : 26-09-2019
+ * Last Modified : 28-10-2019 17:31:22
+ * Created By :
  */
 
 #include"main.h"
 
 void User_Boundaries(double *B)
 {
-   int i, j, k, n, cell;
-
 #if DIM == 2
    
    // Linear extrapolation. Important for the diagonal shock tube
- 
-   #if INTERFACE == DIAGONAL
-   for(i = 0; i <= Nx1; i++)
+   for(int n = 0; n < eq; n++)
    {
-      for(j = 0; j <= Nx2; j++)
+      for(int i = gc; i <= Nx1-gc; i++)
       {
-         for(n = 0; n < eq; n++)
+         for(int j = gc; j <= Nx2-gc; j++)
          {
             if(i > Nx1-gc)
             {
@@ -33,26 +29,21 @@ void User_Boundaries(double *B)
             }
          }
       }
-   }
 
-   for(i = Nx1; i >= 0; i--)
-   {
-      for(j = Nx2; j <= 0; j--)
+      for(int i = Nx1-gc; i >= gc; i--)
       {
-         for(n = 0; n < eq; n++)
+         for(int j = Nx2-gc; j >= gc; j--)
          {
-            if(i > Nx1-gc)
+            if(i < gc)
             {
                B(n,i,j) = B(n,i+2,j) + ((grid.X1[i] - grid.X1[i+2])/(grid.X1[i+1] - grid.X1[i+2]))*(B(n,i+1,j) - B(n,i+2,j)); 
             }
-            if(j > Nx2-gc)
+            if(j < gc)
             {
                B(n,i,j) = B(n,i,j+2) + ((grid.X2[j] - grid.X2[j+2])/(grid.X2[j+1] - grid.X2[j+2]))*(B(n,i,j+1) - B(n,i,j+2)); 
             }
          }
       }
    }
-   #endif 
-   
 #endif
 }
