423ba577e7dc6afa1c4f2b3b8aaeaa758531ed67
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2020-07-13 17:55:32 -0500

    Surface volume

--- Src/physics/RHD/Boyer-Lindquist/metric.c
+++ Src/physics/RHD/Boyer-Lindquist/metric.c
@@ -57,13 +57,22 @@ void Get_Metric_Components(gauge_ *local_grid)
    double theta = local_grid->x[2]; 
    double phi   = local_grid->x[3];
 
+   double cos_theta = cos(theta);
+   double sin_theta = sin(theta);
+
+   if(theta == M_PI)
+   {
+      sin_theta =  0.0;
+      cos_theta = -1.0;
+   }
+
    double Sigma, rho2, Delta, a, M;
 
    a     = Black_Hole_Spin;
    M     = Black_Hole_Mass;
    Delta = r*r - 2.0*M*r + a*a;
-   rho2  = r*r + a*a*cos(theta)*cos(theta);
-   Sigma = pow(r*r + a*a,2.0) - a*a*Delta*sin(theta)*sin(theta);
+   rho2  = r*r + a*a*cos_theta*cos_theta;
+   Sigma = pow(r*r + a*a,2.0) - a*a*Delta*sin_theta*sin_theta;
 
    local_grid->lapse = sqrt(rho2*Delta/Sigma);
 
@@ -81,9 +90,9 @@ void Get_Metric_Components(gauge_ *local_grid)
    local_grid->gamma_con[1][2] = 0.0;
    local_grid->gamma_con[2][0] = 0.0;
    local_grid->gamma_con[2][1] = 0.0;
-   local_grid->gamma_con[2][2] = rho2/(Sigma*sin(theta)*sin(theta));
+   local_grid->gamma_con[2][2] = rho2/(Sigma*sin_theta*sin_theta);
 
-   local_grid->dety = sqrt(Sigma*rho2*sin(theta)*sin(theta)/Delta);
+   local_grid->dety = sqrt(Sigma*rho2*sin_theta*sin_theta/Delta);
 
    #elif POLAR == TRUE
 
@@ -95,7 +104,7 @@ void Get_Metric_Components(gauge_ *local_grid)
    local_grid->gamma_con[0][1] = 0.0;
    local_grid->gamma_con[0][2] = 0.0;
    local_grid->gamma_con[1][0] = 0.0;
-   local_grid->gamma_con[1][1] = rho2/(Sigma*sin(theta)*sin(theta));
+   local_grid->gamma_con[1][1] = rho2/(Sigma*sin_theta*sin_theta);
    local_grid->gamma_con[1][2] = 0.0;
    local_grid->gamma_con[2][0] = 0.0;
    local_grid->gamma_con[2][1] = 0.0;
@@ -217,6 +226,15 @@ void Gauge_Derivatives(der_gauge_ *der, gauge_ *local_grid)
    double theta = local_grid->x[2]; 
    double phi   = local_grid->x[3];
 
+   double cos_theta = cos(theta);
+   double sin_theta = sin(theta);
+
+   if(theta == M_PI)
+   {
+      sin_theta =  0.0;
+      cos_theta = -1.0;
+   }
+
    double Sigma, rho2, Delta, a, M;
    double drho2dr, drho2dt;
    double dSigmadr, dSigmadt;
@@ -225,15 +243,15 @@ void Gauge_Derivatives(der_gauge_ *der, gauge_ *local_grid)
    a     = Black_Hole_Spin;
    M     = Black_Hole_Mass;
    Delta = r*r - 2.0*M*r + a*a;
-   rho2  = r*r + a*a*cos(theta)*cos(theta);
-   Sigma = pow(r*r + a*a,2.0) - a*a*Delta*sin(theta)*sin(theta);
+   rho2  = r*r + a*a*cos_theta*cos_theta;
+   Sigma = pow(r*r + a*a,2.0) - a*a*Delta*sin_theta*sin_theta;
 
    drho2dr = 2.0*r;
    drho2dt = -a*a*sin(2.0*theta);
 
    dDeltadr = 2.0*(r - M);
 
-   dSigmadr = 4.0*r*(r*r + a*a) - a*a*sin(theta)*sin(theta)*dDeltadr;
+   dSigmadr = 4.0*r*(r*r + a*a) - a*a*sin_theta*sin_theta*dDeltadr;
    dSigmadt = -a*a*Delta*sin(2.0*theta);
 
    #if POLAR == FALSE
@@ -260,7 +278,7 @@ void Gauge_Derivatives(der_gauge_ *der, gauge_ *local_grid)
    der->dgam[0][1][2] = 0.0;
    der->dgam[0][2][0] = 0.0;
    der->dgam[0][2][1] = 0.0;
-   der->dgam[0][2][2] = (rho2*dSigmadr - Sigma*drho2dr)*sin(theta)*sin(theta)/(rho2*rho2);
+   der->dgam[0][2][2] = (rho2*dSigmadr - Sigma*drho2dr)*sin_theta*sin_theta/(rho2*rho2);
 
    der->dgam[1][0][0] = drho2dt/Delta;
    der->dgam[1][0][1] = 0.0;
@@ -306,7 +324,7 @@ void Gauge_Derivatives(der_gauge_ *der, gauge_ *local_grid)
    der->dgam[0][2][1] = 0.0;
    der->dgam[0][1][0] = 0.0;
    der->dgam[0][1][2] = 0.0;
-   der->dgam[0][1][1] = (rho2*dSigmadr - Sigma*drho2dr)*sin(theta)*sin(theta)/(rho2*rho2);
+   der->dgam[0][1][1] = (rho2*dSigmadr - Sigma*drho2dr)*sin_theta*sin_theta/(rho2*rho2);
 
    der->dgam[2][0][0] = drho2dt/Delta;
    der->dgam[2][0][2] = 0.0;
--- Src/physics/RHD/surface_volume.c
+++ Src/physics/RHD/surface_volume.c
@@ -10,6 +10,16 @@
 void Surface_Volume()
 {
    int i, j, k;
+   double volimjpkp, volijpkp, volipjpkp;
+   double volimjkp,  volijkp,  volipjkp;
+   double volimjmkp, volijmkp, volipjmkp;
+   double volimjpk,  volijpk,  volipjpk;
+   double volimjk,   volijk,   volipjk;
+   double volimjmk,  volijmk,  volipjmk;
+   double volimjpkm, volijpkm, volipjpkm;
+   double volimjkm,  volijkm,  volipjkm;
+   double volimjmkm, volijmkm, volipjmkm;
+   double dV;
    gauge_ vol, surf_p, surf_m;
 
 #if DIM == 1
@@ -40,6 +50,116 @@ void Surface_Volume()
    {
       for(j = 0; j <= Nx2; j++)
       {
+         // ip jp k
+         vol.x[1]    = grid.X1p[i];
+         vol.x[2]    = grid.X2p[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volipjpk = vol.dety;
+ 
+         // i jp k
+         vol.x[1]    = grid.X1[i];
+         vol.x[2]    = grid.X2p[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volijpk = vol.dety;
+
+         // im jp k
+         vol.x[1]    = grid.X1m[i];
+         vol.x[2]    = grid.X2p[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volimjpk = vol.dety;
+ 
+         // ip j k
+         vol.x[1]    = grid.X1p[i];
+         vol.x[2]    = grid.X2[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volipjk = vol.dety;
+ 
+         // i j k
+         vol.x[1]    = grid.X1[i];
+         vol.x[2]    = grid.X2[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volijk = vol.dety;
+
+         // im j k
+         vol.x[1]    = grid.X1m[i];
+         vol.x[2]    = grid.X2[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volimjk = vol.dety;
+ 
+         // ip jm k
+         vol.x[1]    = grid.X1p[i];
+         vol.x[2]    = grid.X2m[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volipjmk = vol.dety;
+ 
+         // i jm k
+         vol.x[1]    = grid.X1[i];
+         vol.x[2]    = grid.X2m[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volijmk = vol.dety;
+
+         // im jm k
+         vol.x[1]    = grid.X1m[i];
+         vol.x[2]    = grid.X2m[j];
+         #if POLAR == TRUE
+         vol.x[2]    = M_PI_2;
+         #endif
+         vol.x[3]    = 0.0;
+
+         Get_Metric_Components(&vol);
+         volimjmk = vol.dety;
+
+         //dV = (      (volimjpk + 4.0*volijpk + volipjpk)/6.0 \
+               + 4.0*( volimjk + 4.0* volijk +  volipjk)/6.0 \
+               +     (volimjmk + 4.0*volijmk + volipjmk)/6.0)/6.0;
+         //dV = (  4.0*volimjpk +  8.0*volijpk + 4.0*volipjpk \
+               + 8.0*volimjk  + 16.0*volijk  + 8.0*volipjk  \
+               + 4.0*volimjmk +  8.0*volijmk + 4.0*volipjmk)/64;
+         dV = (  1.0*volimjpk +  4.0*volijpk + 1.0*volipjpk \
+               + 4.0*volimjk  + 16.0*volijk  + 4.0*volipjk  \
+               + 1.0*volimjmk +  4.0*volijmk + 1.0*volipjmk)/36.0;
+         //dV = volijk;
+
          vol.x[1]    = grid.X1[i];
          surf_p.x[1] = grid.X1p[i];
          surf_m.x[1] = grid.X1m[i];
@@ -59,8 +179,16 @@ void Surface_Volume()
          Get_Metric_Components(&surf_p);
          Get_Metric_Components(&surf_m);
  
-         S1p(i,j) = surf_p.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
-         S1m(i,j) = surf_m.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
+         //S1p(i,j) = surf_p.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
+         //S1m(i,j) = surf_m.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
+         S1p(i,j) = surf_p.dety/dV;
+         S1m(i,j) = surf_m.dety/dV;
+         //S1p(i,j) = surf_p.dety/vol.dety;
+         //S1m(i,j) = surf_m.dety/vol.dety;
+
+         //printf("\n");
+         //printf("%e %e %e\n",dV,(surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0,volijk);
+         //printf("\n");
  
          vol.x[1]    = grid.X1[i];
          surf_p.x[1] = grid.X1[i];
@@ -81,8 +209,17 @@ void Surface_Volume()
          Get_Metric_Components(&surf_p);
          Get_Metric_Components(&surf_m);
  
-         S2p(i,j) = surf_p.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
-         S2m(i,j) = surf_m.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
+         //S2p(i,j) = surf_p.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
+         //S2m(i,j) = surf_m.dety/((surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0);
+         S2p(i,j) = surf_p.dety/dV;
+         S2m(i,j) = surf_m.dety/dV;
+         //S2p(i,j) = surf_p.dety/vol.dety;
+         //S2m(i,j) = surf_m.dety/vol.dety;
+
+         //printf("\n");
+         //printf("%e %e %e\n",dV,(surf_p.dety + 4.0*vol.dety + surf_m.dety)/6.0,volijk);
+         //printf("\n");
+ 
       }
    }
 
