1dac2cf1244f8ff065f2b0c072bb9943a1a67f08
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2021-02-21 23:25:46 -0600

    Chanes in print_values, io, output

--- Src/common/print_values.c
+++ Src/common/print_values.c
@@ -8,39 +8,94 @@
 
 #include"main.h"
 
-void Print_Values()
+void Print_Values0()
 {
+   char id[50];
+   strcpy(id,"out");
+
    if(graf == 1)
    {
       if(binary == TRUE)
       {
-         Output1_bin(TRUE);
+         Output1_bin(id);
       }
       else
       {
-         Output1(TRUE);
+         Output1(id);
       }
    }
    else if(graf == 2)
    {
       if(binary == TRUE)
       {
-         Output2_bin(TRUE);
+         Output2_bin(id);
       }
       else
       {
-         Output2(TRUE);
+         Output2(id);
       }
    }
    else if(graf == 3)
    {
       if(binary == TRUE)
       {
-         Output3_bin(TRUE);
+         Output3_bin(id);
       }
       else
       {
-         Output3(TRUE);
+         Output3(id);
+      }
+   }
+
+   /**
+    * Forcing termination
+    */
+   Alternative_Termination();
+}
+
+void Print_Values1(char *file_id)
+{
+   char id[50];
+   if (strcmp(file_id,"") == 0)
+   {
+      strcpy(id,"out");
+   }
+   else
+   {
+      strcpy(id,file_id);
+   }
+
+   if(graf == 1)
+   {
+      if(binary == TRUE)
+      {
+         Output1_bin(id);
+      }
+      else
+      {
+         Output1(id);
+      }
+   }
+   else if(graf == 2)
+   {
+      if(binary == TRUE)
+      {
+         Output2_bin(id);
+      }
+      else
+      {
+         Output2(id);
+      }
+   }
+   else if(graf == 3)
+   {
+      if(binary == TRUE)
+      {
+         Output3_bin(id);
+      }
+      else
+      {
+         Output3(id);
       }
    }
 
--- Src/include/io.h
+++ Src/include/io.h
@@ -4,8 +4,25 @@
  * @author Alejandro Aguayo-Ortiz
  *
  * @brief Input and output function and variable definitions.
- *
  */
+
+// Macros that allow you to define Print_Values0, ..., Print_Values5 for 
+// 0, ..., 5 allowed parameters, respectively, and still be called as 
+// Print_Values.
+#define NARGS(...) NARGS_(__VA_ARGS__, 5, 4, 3, 2, 1, 0)
+#define NARGS_(_5, _4, _3, _2, _1, _0, N, ...) N
+
+#define CONC(A, B) CONC_(A, B)
+#define CONC_(A, B) A##B
+
+#define Print_Values(...) CONC(Print_Values, NARGS(__VA_ARGS__))(__VA_ARGS__)
+
+#define Output1(x) _Generic((x), int: Output1_int, char *: Output1_char)(x);
+#define Output2(x) _Generic((x), int: Output2_int, char *: Output2_char)(x);
+#define Output3(x) _Generic((x), int: Output3_int, char *: Output3_char)(x);
+#define Output1_bin(x) _Generic((x), int: Output1_bin_int, char *: Output1_bin_char)(x);
+#define Output2_bin(x) _Generic((x), int: Output2_bin_int, char *: Output2_bin_char)(x);
+#define Output3_bin(x) _Generic((x), int: Output3_bin_int, char *: Output3_bin_char)(x);
 
 //Paramfile
 int binary;
@@ -34,21 +51,28 @@ void Init_Simulation(double *tprint, int itprint);
 
 void Manage_Simulation_Info(int argc, char *argv[]);
 
-void Output1(int itprint);
+void Output1_int(int itprint);
+void Output1_char(char *itprint);
 
-void Output2(int itprint);
+void Output2_int(int itprint);
+void Output2_char(char *itprint);
 
-void Output3(int itprint);
+void Output3_int(int itprint);
+void Output3_char(char *itprint);
 
-void Output1_bin(int itprint);
+void Output1_bin_int(int itprint);
+void Output1_bin_char(char *itprint);
 
-void Output2_bin(int itprint);
+void Output2_bin_int(int itprint);
+void Output2_bin_char(char *itprint);
 
-void Output3_bin(int itprint);
+void Output3_bin_int(int itprint);
+void Output3_bin_char(char *itprint);
 
 void Print_Time_Values(double *tprint, double *dtprint, int itprint);
 
-void Print_Values();
+void Print_Values0();
+void Print_Values1(char *file_id);
 
 void Restart();
 
