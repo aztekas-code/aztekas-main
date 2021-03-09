1248c862c37ef11b407f09dc36511383349eb46e
e-tejeda <39070232+e-tejeda@users.noreply.github.com>
2021-03-09 17:29:35 -0600

    Update user_param.h

--- SIMULATIONS/HD/Self-gravity/user_param.h
+++ SIMULATIONS/HD/Self-gravity/user_param.h
@@ -24,7 +24,7 @@ double density_0, pressure_0, velocity_0, Rad, polyK, r_acc;
 double density_atm, pressure_atm;
 
 // mass that has been accreted (r<r_acc)
-double mass_acc = 0.0;
+double mass_acc;
 // Total gas cloud's mass
 double mass_tot;
 double *mass;
