0604790f2dbfd9a7c35e73124f23167b6ee0e4fc
Alejandro Aguayo-Ortiz <aaguayoo92@ciencias.unam.mx>
2021-03-12 11:19:12 -0600

    Added CO1 and CO2 compositions HELMHOLTZ

--- Src/physics/EOS/Helmholtz/eos.c
+++ Src/physics/EOS/Helmholtz/eos.c
@@ -19,8 +19,9 @@ void EoS(eos_ *eos, double *u, gauge_ *local_grid)
    printf("\n");
    printf("HELHOLTZ_COMPOSITION not defined\n");
    printf("Include in user_param.h one of the following options:\n");
-   printf("   - #define HELHOLTZ_COMPOSITION    DEFAULT -> H, He, C\n");
-   printf("   - #define HELHOLTZ_COMPOSITION    CO      -> C, O\n");
+   printf("   - #define HELHOLTZ_COMPOSITION    DEFAULT -> H (75%), He (23%), C (2%)\n");
+   printf("   - #define HELHOLTZ_COMPOSITION    CO1     -> C (50%), O(50%)\n");
+   printf("   - #define HELHOLTZ_COMPOSITION    CO2     -> C (30%), O(70%)\n");
    printf("\n");
    exit(EXIT_FAILURE);
 #endif
@@ -37,16 +38,28 @@ void EoS(eos_ *eos, double *u, gauge_ *local_grid)
    Z[0] = 1.0;
    Z[1] = 2.0;
    Z[2] = 6.0;
-#elif HELMHOLTZ_COMPOSITION == CO
+#elif HELMHOLTZ_COMPOSITION == CO1
    xMass[0] = 0.0;
-   xMass[1] = 0.23;
-   xMass[2] = 0.02;
+   xMass[1] = 0.50;
+   xMass[2] = 0.50;
 
    A[0] = 0.0;
    A[1] = 12.0;
-   A[2] = 16;
+   A[2] = 16.0;
 
-   Z[0] = ;
+   Z[0] = 0.0;
+   Z[1] = 6.0;
+   Z[2] = 8.0;
+#elif HELMHOLTZ_COMPOSITION == CO2
+   xMass[0] = 0.0;
+   xMass[1] = 0.30;
+   xMass[2] = 0.70;
+
+   A[0] = 0.0;
+   A[1] = 12.0;
+   A[2] = 16.0;
+
+   Z[0] = 0.0;
    Z[1] = 6.0;
    Z[2] = 8.0;
 #endif
