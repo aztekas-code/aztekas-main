ed8c7030dd08bab9c754b213765b6cf5a368c902
e-tejeda <emilio@ciencias.unam.mx>
2021-03-23 12:52:32 -0600

    Dust collapse

---
+++ SIMULATIONS/HD/Dust_collapse/initial.c
@@ -0,0 +1,121 @@
+/*
+ * aztekas initial module
+ * Date of creation/modification: 27-09-19 11:26:00
+ * author: Alejandro Aguayo-Ortiz
+ */
+
+//Do not erase any of these libraries//
+#include"main.h"
+
+void Initial()
+{
+   int n, i, j, k, cell;
+   
+   double r;
+   
+   //Initialize grid.time
+   grid.time = 0.0;
+
+   //Initialize dt
+   dt = 0.0;
+   
+
+#if DIM == 1 
+
+   ///////////////////////////
+   //-------Bondi-1D--------//
+   ///////////////////////////
+   
+   double test, mass_aux;
+   double r1,r2,rho1,rho2,m1,m2;
+   
+   int iRad;
+   
+   r=Rad;
+   test = (Nx1-2*gc)*log(r-x1min+1)/log(x1max-x1min+1) + gc;
+   
+   iRad = (int)floor(test);
+      
+   //printf("%d %f \n",iRad,grid.X1[iRad]);
+   
+   for(i = 0; i <= iRad; i++)
+   {
+   	  r = grid.X1[i];
+   	  U(RHO,i) = density_0;
+   	  U(PRE,i) = pressure_0;
+      U(VX1,i) = 0.0;
+   }
+   
+   // define atmosphere density as 1000 times less 
+   // than the minimum cloud's density 
+   //printf( "%e %e \n", grid.X1[iRad], U(RHO,iRad) );   
+   density_atm = 1.e-3*density_0;
+   pressure_atm = pressure_0;
+
+   for(i = iRad+1; i <= Nx1; i++)
+   {
+      r = grid.X1[i];
+      U(RHO,i) = density_atm;
+      U(PRE,i) = pressure_atm;
+      U(VX1,i) = 0.0;
+   }
+
+   //initialize gas cloud mass vector
+   r1 = grid.X1[gc];
+   mass[gc] = 4.*M_PI*r1*r1*r1*density_0/3. ;
+   //printf( "%e, %e \n", r1, mass[gc] );
+   
+   for(int i = gc+1; i < Nx1-gc; i++)
+   {
+        r1 = grid.X1[i-1];
+        r2 = grid.X1[i];        
+        rho1 = U(RHO,i-1);
+        rho2 = U(RHO,i);        
+        m1 = 4.*M_PI*r1*r1*rho1 ;
+        m2 = 4.*M_PI*r2*r2*rho2 ;
+        mass[i] = mass[i-1] + 0.5*(m1+m2)*(r2-r1);  
+        //printf( "%e %e %e \n", r2, mass[i], m2*r2/3. );
+        //printf( "%e \n", r1 );
+   }
+   //initial gas cloud's mass
+   mass_tot = mass[Nx1-gc-1];
+   //printf( "%e \n", mass_tot );
+   mass_acc = 0.0;
+
+#elif DIM == 2
+
+   ///////////////////////////
+   //-------Bondi-2D--------//
+   ///////////////////////////
+
+   for(i = 0; i <= Nx1; i++)
+   {
+      for(j = 0; j <= Nx2; j++)
+      {
+         U(RHO,i,j) = density_0;
+         U(PRE,i,j) = pressure_0;
+         U(VX1,i,j) = velocity_0;
+         U(VX2,i,j) = 0.0;
+      }
+   }
+
+#elif DIM == 4
+
+   ///////////////////////////
+   //------Bondi-2.5D-------//
+   ///////////////////////////
+
+   for(i = 0; i <= Nx1; i++)
+   {
+      for(j = 0; j <= Nx2; j++)
+      {
+         U(RHO,i,j) = density_0;
+         U(PRE,i,j) = pressure_0;
+         U(VX1,i,j) = velocity_0;
+         U(VX2,i,j) = 0.0;
+         U(VX3,i,j) = 0.0;
+      }
+   }
+
+#endif
+}
---
+++ SIMULATIONS/HD/Dust_collapse/user_boundaries.c
@@ -0,0 +1,79 @@
+/*
+ * aztekas boundaries module
+ * Date of creation/modification: 26-09-19 11:33:21
+ * author: Alejandro Aguayo-Ortiz
+ */
+
+#include"main.h"
+
+void User_Boundaries(double *B)
+{
+#if DIM == 1
+
+   double r, r1,r2,rho1,rho2,m1, m2, mass_aux;
+
+   mass_aux = 0.0;
+   // update mass vector 
+   for(int i = gc-1; i < Nx1-gc; i++)
+   {
+        r1 = grid.X1[i];
+        r2 = grid.X1[i+1];        
+        rho1 = B(RHO,i);
+        rho2 = B(RHO,i+1);        
+        m1 = 4.*M_PI*r1*r1*rho1 ;
+        m2 = 4.*M_PI*r2*r2*rho2 ;
+        mass[i] = mass_aux + 0.5*(m1+m2);  
+        mass_aux = mass[i];
+   }
+   // accreted mass so far
+   mass_acc = mass_tot - mass[Nx1-gc-1];
+   
+   // shift mass vector by mass_acc
+    for(int i = gc-1; i < Nx1-gc; i++)
+   {
+        mass[i] = mass[i] + mass_acc;
+   }  
+
+  
+   for(int i = Nx1-gc; i <= Nx1; i++)
+   {
+         B(RHO,i) = density_atm;
+         B(PRE,i) = pressure_atm;
+         B(VX1,i) = 0.0;
+   }
+
+#elif DIM == 2
+
+   for(int j = 0; j <= Nx2; j++)
+   {
+      for(int i = 0; i <= Nx1; i++)
+      {
+         if(i >= Nx1-gc)
+         {
+            B(RHO,i,j) = density_0;
+            B(PRE,i,j) = pressure_0;
+            B(VX1,i,j) = velocity_0q;
+            B(VX2,i,j) = 0.0;
+         }
+      }
+   }
+
+#elif DIM == 4
+
+   for(int j = 0; j <= Nx2; j++)
+   {
+      for(int i = 0; i <= Nx1; i++)
+      {
+         if(i >= Nx1-gc)
+         {
+            B(RHO,i,j) = density_0;
+            B(PRE,i,j) = pressure_0;
+            B(VX1,i,j) = velocity_0;
+            B(VX2,i,j) = 0.0;
+            B(VX3,i,j) = 0.0;
+         }
+      }
+   }
+
+#endif
+}
